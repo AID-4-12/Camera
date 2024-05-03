e07fc6e0bac9924c58566014af72d37b2ad241dc
imnyang <me@imnyang.xyz>
2024-05-04 01:26:17 +0900

    Changes to be committed: 	modified:   live.py 	modified:   run_live_server.sh

--- run_live_server.sh
+++ run_live_server.sh
@@ -1,1 +1,1 @@
-libcamera-vid -n -t 0 --width 1280 --height 960 --framerate 24 --inline --listen -o tcp://0.0.0.0:8888
+libcamera-vid -t 0 --inline -o - | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8080/}'
